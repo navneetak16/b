93e13163ef2f44c89c7be81aed8fcf7bc61c2e11
Navneet Yadav <125375766+navneetak16@users.noreply.github.com>
2025-10-06 21:21:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ✅ Replace with your full dump values
+// ✅ Replace
 const customData = {
   name: "PiyushJoshi",
   "group": "admin",
