6f41a66bd62d5854add06983628c2b03a2b828ff
Navneet Yadav <125375766+navneetak16@users.noreply.github.com>
2025-10-07 21:55:35 +0530

    Update server.js

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -13,7 +13,9 @@ const customData = {
   levelInfo: {"id": 10000},
   level: 10000,
   identities: [{ "type": "guest", "id": "34361252775602123902264349160523", "createTime": "2025-08-14T05:57:28.274Z"}],
-  equipped:{
+  progressions: [{"userId": 3902264349160523, "progressionId": "credit-points", "type": 2, "stats": "{ \"CP\" : 0 }", "currentLevelIndex": 0},
+        { "userId": 3902264349160523,  "progressionId": "ranked-s3-stage3", "type": 1, "stats": "{ \"drr\" : 0 }", "currentLevelIndex": 8 }],
+   equipped:{
     "profile.avatar": [{id: "char_droid_01_skin_0002" }],
     "vehicle.veh_bike_01":[{"id": "veh_bike_01_skin_0053" }],
     trails:[{"id": "trl_0064" }],
@@ -297,6 +299,7 @@ app.all("*", async (req, res) => {
           json.equipped["vehicle.veh_bike_01"] = customData.equipped["vehicle.veh_bike_01"];
           json.equipped["weapon.gun_mle_01"] = customData.equipped["weapon.gun_mle_01"];
           json.level = customData.level;
+          json.progressions = customData.progressions;
           json.identities = customData.identities;
           json.equipped.trails = customData.equipped.trails;
           json.equipped["slotwheel_slot_0001"] = customData.equipped["slotwheel_slot_0001"];
